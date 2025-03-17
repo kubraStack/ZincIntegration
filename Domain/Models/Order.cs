d8d855abeedd27c934ecd39be76b76784ae032b2
kubrastack <kmrt3014@gmail.com>
2025-03-17 22:45:17 +0300

    added layers and entity model

---
+++ Domain/Models/Order.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EntityLayer.Entities
+{
+    public class Order
+    {
+        public int Id { get; set; } //Primary key
+        public string ProductId { get; set; }
+        public string Platform { get; set; } //amazon, ebay
+        public string CustomerName { get; set; }
+        public string ShippingAddress { get; set; }
+        public DateTime OrderDate { get; set; }
+        public string Status { get; set; } //pending, completed, failed
+    }
+}
---
+++ Domain/Models/OrderResponseDto.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EntityLayer.Entities
+{
+    public class OrderResponseDto
+    {
+        public bool Success { get; set; }
+        public string Message { get; set; }
+        public int? OrderId { get; set; }
+    }
+}
